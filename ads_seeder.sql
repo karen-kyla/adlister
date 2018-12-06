2d6c869a0f3e8db907217f113f66375d1e26b476
Kyla Allen <themightyflorp@Kylas-MacBook-Air.local>
2018-12-06 10:48:31 -0600

    added the seeders

---
+++ ads_seeder.sql
@@ -0,0 +1,5 @@
+Use adlister;
+
+INSERT into ads (user_id, title, description, cat_id) VALUES
+(3, 'Trumpet for Sale', 'A three valve brass instrument only used twice. Yamaha gold. So call me', 3 ),
+(1, 'Dog walking services', 'I will walk your dog. No pay needed. I just like dogs.', 5 );
---
+++ categories_seeder.sql
@@ -0,0 +1,8 @@
+USE adlister;
+
+INSERT into categories (cat_name) VALUES
+('jobs'),
+('nature'),
+('musical'),
+('automobile'),
+('animals');
